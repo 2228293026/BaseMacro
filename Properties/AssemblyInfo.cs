4f7774229e574a434b4f2452e36e66c5eaf20dea
HitMargin <hitmargin@qq.com>
2026-02-25 18:03:10 +0800

    VERSION

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //      修订号
 //
 [assembly: AssemblyVersion("1.0.6.0")]
-[assembly: AssemblyFileVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.61.0")]
