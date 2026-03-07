76c09d9d88ed53108b163e8def614e2628ea5fb2
HitMargin <142143104+2228293026@users.noreply.github.com>
2026-03-07 10:56:05 +0800

    Use game SkyHook types and union-backed event fields

--- Macro/SkyHookSystem.cs
+++ Macro/SkyHookSystem.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Threading;
 
 #nullable enable
 
@@ -11,125 +9,63 @@ namespace BaseMacro.Macro
     #region SkyHook 系统独立实现
 
     /// <summary>
-    /// SkyHook 输入系统实现
+    /// SkyHook 兼容层：按键结构改为对齐游戏内置 SkyHook 类型。
     /// </summary>
     public static class SkyHookSystem
     {
-        /// <summary>
-        /// SkyHook 事件类型
-        /// </summary>
-        public enum EventType : uint
-        {
-            KeyPressed = 0x100,
-            KeyReleased = 0x101,
-            MouseMoved = 0x200,
-            MouseButton = 0x201
-        }
+        private static readonly long EpochTicks = new DateTime(1970, 1, 1).Ticks;
 
         /// <summary>
-        /// SkyHook 按键标签
+        /// SkyHook 事件结构（与游戏内结构对齐，Label/Key 使用 Union 共享内存）
         /// </summary>
-        public enum KeyLabel : uint
-        {
-            None = 0,
-            Escape = 0x1B,
-            Alpha0 = 0x30, Alpha1, Alpha2, Alpha3, Alpha4, Alpha5, Alpha6, Alpha7, Alpha8, Alpha9,
-            A = 0x41, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z,
-            F1 = 0x70, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12,
-            Left = 0x25, Up, Right, Down,
-            Space = 0x20, Return = 0x0D, Tab = 0x09,
-            LControl = 0xA2, RControl = 0xA3,
-            LShift = 0xA0, RShift = 0xA1,
-            LAlt = 0xA4, RAlt = 0xA5
-        }
-
-        /// <summary>
-        /// SkyHook 事件结构（完整版）
-        /// </summary>
-        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        [StructLayout(LayoutKind.Explicit, Pack = 1)]
         public struct SkyHookEvent
         {
-            public long TimeSec;           // 秒
-            public uint TimeSubsecNano;     // 纳秒
-            public EventType Type;           // 事件类型
-            public KeyLabel Label;           // 按键标签
-            public ushort Key;               // 按键码
-            public uint Flags;                // 标志位
-            public uint ExtraInfo;            // 额外信息
+            [FieldOffset(0)]
+            public long TimeSec;
+
+            [FieldOffset(8)]
+            public uint TimeSubsecNano;
+
+            [FieldOffset(12)]
+            public global::SkyHook.EventType Type;
+
+            // Union：Label 与 Key 共享同一段内存
+            [FieldOffset(16)]
+            public global::SkyHook.KeyLabel Label;
+
+            [FieldOffset(16)]
+            public ushort Key;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public long GetTimeInTicks() =>
+                TimeSec * 10000000L + (long)(TimeSubsecNano / 100U) + EpochTicks;
 
             /// <summary>
-            /// 创建 SkyHook 事件
+            /// 创建 SkyHook 事件。
+            /// elapsedTicks 输入单位为 100ns（DateTime ticks）。
             /// </summary>
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static SkyHookEvent Create(byte keyCode, bool isDown, long elapsedTicks)
             {
                 var (sec, nano) = ConvertTicksToSkyHookTime(elapsedTicks);
-
                 return new SkyHookEvent
                 {
                     TimeSec = sec,
                     TimeSubsecNano = nano,
-                    Type = isDown ? EventType.KeyPressed : EventType.KeyReleased,
-                    Label = GetKeyLabel(keyCode),
-                    Key = keyCode,
-                    Flags = 0,
-                    ExtraInfo = 0
+                    Type = isDown ? global::SkyHook.EventType.KeyPressed : global::SkyHook.EventType.KeyReleased,
+                    Key = keyCode
                 };
             }
 
-            /// <summary>
-            /// 将 100ns ticks 转换为 SkyHook 时间格式
-            /// </summary>
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             private static (long sec, uint nano) ConvertTicksToSkyHookTime(long ticks)
             {
-                // Windows Epoch (1601-01-01) 到 Unix Epoch (1970-01-01) 的差值
-                const long EPOCH_DIFFERENCE = 116444736000000000; // 100-ns 间隔数
-
-                // 转换为从1601开始的100-ns间隔数
-                long windowsTicks = ticks + EPOCH_DIFFERENCE;
-
-                // 转换为秒和纳秒
-                long totalNano = windowsTicks * 100; // 100-ns -> ns
-                long sec = totalNano / 1000000000;
-                uint nano = (uint)(totalNano % 1000000000);
-
+                long sec = ticks / 10000000L;
+                uint nano = (uint)((ticks % 10000000L) * 100);
                 return (sec, nano);
             }
 
-            /// <summary>
-            /// 获取按键对应的标签
-            /// </summary>
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            private static KeyLabel GetKeyLabel(byte keyCode)
-            {
-                if (keyCode >= 0x30 && keyCode <= 0x39)
-                    return (KeyLabel)((int)KeyLabel.Alpha0 + (keyCode - 0x30));
-
-                if (keyCode >= 0x41 && keyCode <= 0x5A)
-                    return (KeyLabel)((int)KeyLabel.A + (keyCode - 0x41));
-
-                if (keyCode >= 0x70 && keyCode <= 0x7B)
-                    return (KeyLabel)((int)KeyLabel.F1 + (keyCode - 0x70));
-
-                return keyCode switch
-                {
-                    0x20 => KeyLabel.Space,
-                    0x0D => KeyLabel.Return,
-                    0x09 => KeyLabel.Tab,
-                    0x25 => KeyLabel.Left,
-                    0x26 => KeyLabel.Up,
-                    0x27 => KeyLabel.Right,
-                    0x28 => KeyLabel.Down,
-                    0xA2 => KeyLabel.LControl,
-                    0xA3 => KeyLabel.RControl,
-                    0xA0 => KeyLabel.LShift,
-                    0xA1 => KeyLabel.RShift,
-                    0xA4 => KeyLabel.LAlt,
-                    0xA5 => KeyLabel.RAlt,
-                    _ => KeyLabel.None
-                };
-            }
         }
 
         /// <summary>
@@ -179,5 +115,6 @@ namespace BaseMacro.Macro
             public ushort wParamH;
         }
     }
+
     #endregion
 }
